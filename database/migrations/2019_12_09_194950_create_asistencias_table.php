c153abd074f357e219095cd26b86bc053129dfe7
ulymay <ulymay@gmail.com>
2019-12-09 20:09:22 +0000

    Se Crearon las tablas y modelos

---
+++ database/migrations/2019_12_09_194950_create_asistencias_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAsistenciasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asistencias', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('falta');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asistencias');
+    }
+}
